29f2d832ef5983d2e9868d985c74f96f2e01efee
Mariah Inada <mariahclarainada@gmail.com>
2025-08-29 17:00:26 -0300

    feat: adiciona teste de saídas

--- cypress/e2e/devfinance/finances.cy.js
+++ cypress/e2e/devfinance/finances.cy.js
@@ -2,16 +2,27 @@
 
 describe('Agilizei - Dev Finances', () => {
   beforeEach(() => {
-    cy.visit('https://devfinance-agilizei.netlify.app/');
-  });
+    cy.visit('https://devfinance-agilizei.netlify.app/')
+    cy.get('#data-table tbody tr').should('have.length', 0)
+  })
 
   it("cadastrar entradas", () => {
-    cy.get('#transaction > .button').click(); // id + classe
-    cy.get('#description').type('Salário'); // id
-    cy.get('[name=amount]').type(5000); // atributos
-    cy.get('[name=date]').type('2025-08-29'); // atributos
-    cy.get('button').contains('Salvar').click(); // tipo e valor
+    cy.get('#transaction > .button').click() // id + classe
+    cy.get('#description').type('Salário') // id
+    cy.get('[name=amount]').type(5000) // atributos
+    cy.get('[name=date]').type('2025-08-29') // atributos
+    cy.get('button').contains('Salvar').click() // tipo e valor
 
     cy.get('#data-table tbody tr').should('have.length', 1) // quantidade de linhas na tabela
-    });
-});
+    })
+
+    it("cadastrar saídas", () => {
+    cy.get('#transaction > .button').click() // id + classe
+    cy.get('#description').type('Presente') // id
+    cy.get('[name=amount]').type(-200) // atributos
+    cy.get('[name=date]').type('2025-08-29') // atributos
+    cy.get('button').contains('Salvar').click() // tipo e valor
+
+    cy.get('#data-table tbody tr').should('have.length', 1) // quantidade de linhas na tabela
+    })
+})
