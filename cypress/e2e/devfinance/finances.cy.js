2acb69574978d4c0e809cc3c48b2f441c98c6087
Mariah Inada <mariahclarainada@gmail.com>
2025-08-29 17:19:49 -0300

    feat: adiciona teste removendo entradas e saídas

--- cypress/e2e/devfinance/finances.cy.js
+++ cypress/e2e/devfinance/finances.cy.js
@@ -6,23 +6,52 @@ describe('Agilizei - Dev Finances', () => {
     cy.get('#data-table tbody tr').should('have.length', 0)
   })
 
-  it("cadastrar entradas", () => {
+  it('cadastrar entradas', () => {
     cy.get('#transaction > .button').click() // id + classe
     cy.get('#description').type('Salário') // id
+    cy.get('[name=amount]').type(5000); // atributos
+    cy.get('[name=date]').type('2025-08-29'); // atributos
+    cy.get('button').contains('Salvar').click(); // tipo e valor
+
+    cy.get('#data-table tbody tr').should('have.length', 1); // quantidade de linhas na tabela
+  });
+
+  it('cadastrar saídas', () => {
+    cy.get('#transaction > .button').click(); // id + classe
+    cy.get('#description').type('Presente'); // id
+    cy.get('[name=amount]').type(-200); // atributos
+    cy.get('[name=date]').type('2025-08-29'); // atributos
+    cy.get('button').contains('Salvar').click(); // tipo e valor
+
+    cy.get('#data-table tbody tr').should('have.length', 1); // quantidade de linhas na tabela
+  });
+
+  it('remover entradas e saídas', () => {
+    const entrada = 'Salário'
+    const saida = 'Presente'
+
+    cy.get('#transaction > .button').click(); // id + classe
+    cy.get('#description').type(entrada); // id
     cy.get('[name=amount]').type(5000) // atributos
     cy.get('[name=date]').type('2025-08-29') // atributos
     cy.get('button').contains('Salvar').click() // tipo e valor
 
-    cy.get('#data-table tbody tr').should('have.length', 1) // quantidade de linhas na tabela
-    })
-
-    it("cadastrar saídas", () => {
     cy.get('#transaction > .button').click() // id + classe
-    cy.get('#description').type('Presente') // id
+    cy.get('#description').type(saida) // id
     cy.get('[name=amount]').type(-200) // atributos
     cy.get('[name=date]').type('2025-08-29') // atributos
     cy.get('button').contains('Salvar').click() // tipo e valor
 
-    cy.get('#data-table tbody tr').should('have.length', 1) // quantidade de linhas na tabela
-    })
+    cy.get('td.description')
+    .contains(entrada)
+    .parent()
+    .find('img[onclick*=remove]')
+    .click()
+
+    cy.get('td.description')
+    .contains(saida)
+    .siblings('td')
+    .children('img[onclick*=remove]')
+    .click()
+  })
 })
