68d698311fc374dda63da80e376d7be78617f4da
Mariah Inada <mariahclarainada@gmail.com>
2025-09-07 18:46:48 -0300

    refactor: reduz o código no login

--- cypress/e2e/saucedemo/login/login.cy.js
+++ cypress/e2e/saucedemo/login/login.cy.js
@@ -2,24 +2,15 @@
 
 describe('testes funcional de login', () => {
     it('deve realizar o login com sucesso', () => {
-        cy.visit('https://www.saucedemo.com/v1/')
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('#login-button').click()
+        cy.login('standard_user','secret_sauce')
         cy.get('.product_label').should('contain', 'Products')
     })
     it('deve validar usuário incorreto', () => {
-        cy.visit('https://www.saucedemo.com/v1/')
-        cy.get('[data-test="username"]').type('standard_user1')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('#login-button').click()
+        cy.login('incorreto','secret_sauce')
         cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service')
     })
     it('deve validar senha incorreta', () => {
-        cy.visit('https://www.saucedemo.com/v1/')
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce1')
-        cy.get('#login-button').click()
+        cy.login('standard_user','incorreto')   
         cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service')
     })
     
---
+++ cypress/support/login_commands.js
@@ -0,0 +1,8 @@
+/// reference types='cypress' />
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('https://www.saucedemo.com/v1/')
+    cy.get('[data-test="username"]').type(username)
+    cy.get('[data-test="password"]').type(password)
+    cy.get('#login-button').click()
+})
