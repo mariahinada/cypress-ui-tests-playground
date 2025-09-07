84dca93380fe5fd3064d6feb219f7ac6b524dc73
Mariah Inada <mariahclarainada@gmail.com>
2025-09-07 17:19:06 -0300

    update: ajuste de estrutura

--- cypress/e2e/devfinance/finances.cy.js
+++ cypress/e2e/devfinance/finances.cy.js
@@ -9,29 +9,29 @@ describe('Agilizei - Dev Finances', () => {
   it('cadastrar entradas', () => {
     cy.get('#transaction > .button').click() // id + classe
     cy.get('#description').type('Salário') // id
-    cy.get('[name=amount]').type(5000); // atributos
-    cy.get('[name=date]').type('2025-08-29'); // atributos
-    cy.get('button').contains('Salvar').click(); // tipo e valor
+    cy.get('[name=amount]').type(5000) // atributos
+    cy.get('[name=date]').type('2025-08-29') // atributos
+    cy.get('button').contains('Salvar').click() // tipo e valor
 
-    cy.get('#data-table tbody tr').should('have.length', 1); // quantidade de linhas na tabela
-  });
+    cy.get('#data-table tbody tr').should('have.length', 1) // quantidade de linhas na tabela
+  })
 
   it('cadastrar saídas', () => {
-    cy.get('#transaction > .button').click(); // id + classe
-    cy.get('#description').type('Presente'); // id
-    cy.get('[name=amount]').type(-200); // atributos
-    cy.get('[name=date]').type('2025-08-29'); // atributos
-    cy.get('button').contains('Salvar').click(); // tipo e valor
+    cy.get('#transaction > .button').click() // id + classe
+    cy.get('#description').type('Presente') // id
+    cy.get('[name=amount]').type(-200) // atributos
+    cy.get('[name=date]').type('2025-08-29') // atributos
+    cy.get('button').contains('Salvar').click() // tipo e valor
 
-    cy.get('#data-table tbody tr').should('have.length', 1); // quantidade de linhas na tabela
-  });
+    cy.get('#data-table tbody tr').should('have.length', 1) // quantidade de linhas na tabela
+  })
 
   it('remover entradas e saídas', () => {
     const entrada = 'Salário'
     const saida = 'Presente'
 
-    cy.get('#transaction > .button').click(); // id + classe
-    cy.get('#description').type(entrada); // id
+    cy.get('#transaction > .button').click() // id + classe
+    cy.get('#description').type(entrada) // id
     cy.get('[name=amount]').type(5000) // atributos
     cy.get('[name=date]').type('2025-08-29') // atributos
     cy.get('button').contains('Salvar').click() // tipo e valor
@@ -43,15 +43,38 @@ describe('Agilizei - Dev Finances', () => {
     cy.get('button').contains('Salvar').click() // tipo e valor
 
     cy.get('td.description')
-    .contains(entrada)
-    .parent()
-    .find('img[onclick*=remove]')
-    .click()
+      .contains(entrada)
+      .parent()
+      .find('img[onclick*=remove]')
+      .click()
 
     cy.get('td.description')
-    .contains(saida)
-    .siblings('td')
-    .children('img[onclick*=remove]')
-    .click()
+      .contains(saida)
+      .siblings('td')
+      .children('img[onclick*=remove]')
+      .click()
+  })
+
+  it.only('validar saldo com diversas transações', () => {
+    const entrada = 'Salário'
+    const saida = 'Presente'
+    cy.get('#transaction > .button').click() // id + classe
+    cy.get('#description').type(entrada) // id
+    cy.get('[name=amount]').type(5000) // atributos
+    cy.get('[name=date]').type('2025-08-29') // atributos
+    cy.get('button').contains('Salvar').click() // tipo e valor
+
+    cy.get('#transaction > .button').click() // id + classe
+    cy.get('#description').type(saida) // id
+    cy.get('[name=amount]').type(-200) // atributos
+    cy.get('[name=date]').type('2025-08-29') // atributos
+    cy.get('button').contains('Salvar').click() // tipo e valor
+
+    cy.get('#data-table tbody tr').each(($el, index, $list) => {
+      cy.get($el)
+        .find('td.income, td.expense')
+        .invoke('text')
+        .then((text) => {})
+    })
   })
 })
